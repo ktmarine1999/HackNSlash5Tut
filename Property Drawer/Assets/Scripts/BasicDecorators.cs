8024a5f12bf5efe26816cb6d05e054cabb1d9e52
James LaFritz <renegadejl@hotmail.com>
2015-06-07 16:33:11 -0400

    A small project playing with different attributes for the way things are displayed in the inspector

---
+++ Property Drawer/Assets/Scripts/BasicDecorators.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicDecorators : MonoBehaviour
+{
+	// Attribute used to make a float or int variable in a script be restricted to a specific range.
+	// When this attribute is used, the float or int will be shown as a slider in the Inspector instead of the default number field.
+	[Range(0, 100)]
+	public int
+		health;
+
+	// Use this PropertyAttribute to add a header above some fields in the Inspector.
+	// The header is done using a DecoratorDrawer.
+	[Header("Currency")]
+	public int
+		gold;
+	public int silver;
+	public int copper;
+
+	// Use this PropertyAttribute to add a header above some fields in the Inspector.
+	// The header is done using a DecoratorDrawer.
+	[Header("Stats")]
+	public int
+		strength;
+	public int dexterity;
+	public int constitution;
+
+	// Use this PropertyAttribute to add some spacing in the Inspector.
+	// The spacing is done using a DecoratorDrawer.
+	[Space(15)]
+	public string
+		race;
+	public int age;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
