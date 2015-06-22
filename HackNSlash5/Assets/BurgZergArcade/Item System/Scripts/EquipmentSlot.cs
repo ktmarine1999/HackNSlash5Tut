736081241ac0feac2b4d2cc1dab3b505109bad75
James LaFritz <renegadejl@hotmail.com>
2015-06-21 22:50:32 -0400

    Changed to from equipmentSlot type to enum for use in the database

---
+++ HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/EquipmentSlot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BurgZergArcade.ItemSystem
+{
+	[System.Serializable]
+	public enum EquipmentSlot
+	{
+		Head,
+		Torso,
+		Legs,
+		Hands,
+		Feet,
+		Necklace,
+		Earings,
+		Rings
+	}
+}
