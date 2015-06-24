801842ec8c53b74d0b6bbfe20453f2c0ac7f5e0a
James LaFritz <renegadejl@hotmail.com>
2015-06-24 14:55:29 -0400

    Got the weapon to display in the list view

--- HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/Editor/Object Editor/ItemObjectListView.cs
+++ HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/Editor/Object Editor/ItemObjectListView.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using BurgZergArcade.Editor;
 
 namespace BurgZergArcade.ItemSystem.Editor
 {
@@ -12,6 +13,8 @@ namespace BurgZergArcade.ItemSystem.Editor
 		Vector2 _scrollPos = Vector2.zero;
 
 		int _listViewWidth = 200;
+		int _listViewButtonWidth = 190;
+		int _listViewButtonHeight = 25;
 
 		/// <summary>
 		/// List all of the Items in the database
@@ -21,8 +24,14 @@ namespace BurgZergArcade.ItemSystem.Editor
 			// Contain the Item Object in a scroll view
 			_scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, "Box", GUILayout.ExpandHeight(true), GUILayout.Width(_listViewWidth));
 
-			// Display the Item Objects in the database
+			// Display the Weapon Objects in the database
+			for(int cnt = 0; cnt < DatabaseManager.weaponDatabase.Count; cnt++)
+			{
+				if(GUILayout.Button(DatabaseManager.weaponDatabase.Get(cnt).name, "Box", GUILayout.Width(_listViewButtonWidth), GUILayout.Height(_listViewButtonHeight)))
+				{
 
+				}
+			}
 
 			// end the Scroll view
 			EditorGUILayout.EndScrollView();
