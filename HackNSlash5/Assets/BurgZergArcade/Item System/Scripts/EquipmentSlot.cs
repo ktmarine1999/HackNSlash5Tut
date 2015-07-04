072e7e104740f5e8aeb7fefa512364098d17834c
James LaFritz <renegadejl@hotmail.com>
2015-07-04 15:31:52 -0400

    Updated Equipment slots

--- HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/EquipmentSlot.cs
+++ HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/EquipmentSlot.cs
@@ -6,13 +6,17 @@ namespace BurgZergArcade.ItemSystem
 	[System.Serializable]
 	public enum EquipmentSlot
 	{
-		Head,
-		Torso,
-		Legs,
-		Hands,
-		Feet,
-		Necklace,
-		Earings,
-		Rings
+		None,
+		Weapon_OneHanded_MainHand,
+		Weapon_OneHanded_OffHand,
+		Weapon_TwoHanded,
+		Armor_Head,
+		Armor_Torso,
+		Armor_Legs,
+		Armor_Hands,
+		Armor_Feet,
+		Armor_Necklace,
+		Armor_Earings,
+		Armor_Rings,
 	}
 }
