b2df07ed454923026ff066ce9186ab0f43671fac
James LaFritz <renegadejl@hotmail.com>
2015-07-05 01:09:47 -0400

    SerializeField myName

--- Property Drawer/Assets/Scripts/BasicDecorators.cs
+++ Property Drawer/Assets/Scripts/BasicDecorators.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class BasicDecorators : MonoBehaviour
 {
+	// Attribute used to Force Unity to serialize a private field.
+	// Serialized fields also show up in the inspector in Unity
+	[SerializeField]
+	private string
+		myName = "Name Me";
+
 	// Attribute used to make a float or int variable in a script be restricted to a specific range.
 	// When this attribute is used, the float or int will be shown as a slider in the Inspector instead of the default number field.
 	[Range(0, 100)]
