c11beb99742bb66852560df5935295692da3b62f
James LaFritz <renegadejl@hotmail.com>
2015-07-05 02:12:52 -0400

    Allow an runtime class method to be initialized when Unity game loads runtime without action from the user.

---
+++ Property Drawer/Assets/Scripts/DemoClassTwo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoClassTwo
+{
+	// Allow an runtime class method to be initialized when Unity game loads runtime without action from the user.
+	[RuntimeInitializeOnLoadMethod]
+	static void RunMeAtLoadTime()
+	{
+		Debug.Log("RunMeAtLoadTime - [RuntimeInitializeOnLoadMethod]");
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		Debug.Log("DemoClassTwo - Start");
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log("DemoClassTwo - Update");
+	}
+}
