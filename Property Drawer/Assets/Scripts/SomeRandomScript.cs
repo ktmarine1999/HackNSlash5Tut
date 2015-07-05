cb938c71394a97a947526fd5e0da788d9e500c0e
James LaFritz <renegadejl@hotmail.com>
2015-07-05 02:00:46 -0400

    DisallowMultipleComponent

---
+++ Property Drawer/Assets/Scripts/SomeRandomScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Prevents MonoBehaviour of same type (or subtype) to be added more than once to a GameObject.
+[DisallowMultipleComponent]
+public class SomeRandomScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
