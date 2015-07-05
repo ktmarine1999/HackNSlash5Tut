d4e8623c1ce354fe6101cbf6d0cfe868a47e9226
James LaFritz <renegadejl@hotmail.com>
2015-07-05 12:01:01 -0400

    ContextMenu

---
+++ Property Drawer/Assets/Scripts/MyContextMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The ContextMenu attribute allows you to add commands to the context menu.
+/// In the inspector of the attached script. When the user selects the context menu, the function will be executed.
+/// This is most useful for automatically setting up scene data from the script. The function has to be non-static.
+/// </summary>
+public class MyContextMenu : MonoBehaviour
+{
+
+	public int lives = 10;
+
+	[ContextMenu("Add A Life")]
+	void AddMethod()
+	{
+		Debug.Log("Add Option of the Context Menu");
+		lives++;
+	}
+
+	[ContextMenu("Reset Lives")]
+	void ResetMethod()
+	{
+		Debug.Log("Resetting lives to 0");
+		lives = 0;
+	}
+
+	[ContextMenu("Remove A Life")]
+	void RemoveMethod()
+	{
+		Debug.Log("Remove Option of the Context Menu");
+		lives--;
+	}
+}
