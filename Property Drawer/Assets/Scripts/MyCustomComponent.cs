81f3c4ee82c9816c5b27e9b9be152f3829fb4841
James LaFritz <renegadejl@hotmail.com>
2015-07-05 12:21:04 -0400

    AddComponentMenu

---
+++ Property Drawer/Assets/Scripts/MyCustomComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The AddComponentMenu attribute allows you to place a script anywhere in the "Component" menu,
+// instead of just the "Component->Scripts" menu.
+// You use this to organize the Component menu better, this way improving workflow when adding scripts.
+// Important notice: You need to restart.
+[AddComponentMenu("BurgZerg Arcade/My Custom Component")]
+public class MyCustomComponent : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
