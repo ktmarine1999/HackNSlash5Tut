dda0c675b80ac7839f8f6bcf5a2f78d0dfbeac8e
James LaFritz <renegadejl@hotmail.com>
2015-07-05 02:04:35 -0400

    RequireComponent

--- Property Drawer/Assets/Scripts/SomeRandomScript.cs
+++ Property Drawer/Assets/Scripts/SomeRandomScript.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 // Prevents MonoBehaviour of same type (or subtype) to be added more than once to a GameObject.
 [DisallowMultipleComponent]
+// The RequireComponent attribute lets automatically add required component as a dependency.
+// When you add a script which uses RequireComponent, 
+// the required component will automatically be added to the game object.
+// This is useful to avoid setup errors.
+// For example a script might require that a rigid body is always added to the same game object.
+// Using RequireComponent this will be done automatically, thus you can never get the setup wrong.
+[RequireComponent(typeof(Rigidbody))]
 public class SomeRandomScript : MonoBehaviour
 {
 
