0c11a22267503518102c17c613c7f792d4bb2796
James LaFritz <renegadejl@hotmail.com>
2015-07-20 01:36:07 -0400

    Add a demo for spawning created weapons into unity

---
+++ HackNSlash5/Assets/Scenes/Demo1/Scripts/Demo1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using BurgZergArcade.ItemSystem;
+
+[DisallowMultipleComponent]
+public class Demo1 : MonoBehaviour
+{
+	// Connect to the database
+	// Spawn from the databasae
+
+	public ISWeaponDatabase database;
+
+	void OnGUI()
+	{
+		for(int cnt =0; cnt < database.Count; cnt++)
+			if(GUILayout.Button("Spawn " + database.Get(cnt).name))
+				Spawn(cnt);
+	}
+
+	void Spawn(int index)
+	{
+		// Get the weapon we want to spawn from the database
+		ISWeapon isw = database.Get(index);
+
+		// Instantuate the weapon in the game world
+		GameObject weaponGO = Instantiate(isw.prefab);
+		// Set the gameobjects name
+		weaponGO.name = isw.name;
+		// Add the Weaopn script
+		Weapon weapon = weaponGO.AddComponent<Weapon>();
+
+		// Transfer the values to the game world weapon
+
+		// Set the weapons Icon
+		weapon.icon = isw.icon;
+		weapon.itemValue = isw.itemValue;
+		weapon.burden = isw.burden;
+		weapon.quality = isw.quality.Icon;
+		weapon.minDamage = isw.minDamage;
+		weapon.durability = isw.durability;
+		weapon.maxDurability = isw.maxDurability;
+		weapon.equipmentSlot = isw.equipmentSlot;
+	}
+}
---
+++ HackNSlash5/Assets/Scenes/Demo1/Scripts/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class Weapon : MonoBehaviour
+{
+	public Sprite icon;
+
+	public int itemValue;
+
+	public int burden;
+
+	public Sprite quality;
+
+	public int minDamage;
+
+	public int durability;
+
+	public int maxDurability;
+
+	public BurgZergArcade.ItemSystem.EquipmentSlot equipmentSlot;
+}
