d675f8484c8361ba068fe1a9a8c3947362b46361
James LaFritz <renegadejl@hotmail.com>
2015-07-30 21:37:37 -0400

    clean up

--- HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/Editor/Object Editor/ItemObjectListView.cs
+++ HackNSlash5/Assets/BurgZergArcade/Item System/Scripts/Editor/Object Editor/ItemObjectListView.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-
-using UnityEngine;
 using UnityEditor;
 using BurgZergArcade.Editor;
 
@@ -13,6 +10,15 @@ namespace BurgZergArcade.ItemSystem.Editor
         int _listViewButtonWidth = 190;
         int _listViewButtonHeight = 25;
 
+        /// <summary>
+        /// List all of the Items in the passed in database.
+        /// </summary>
+        /// <typeparam name="T">The type of item that this database contains</typeparam>
+        /// <param name="database">The database that you want to list</param>
+        /// <param name="dbo">ref to an Item to </param>
+        /// <param name="listScrollPosition">the scroll position for displaying the list</param>
+        /// <param name="selectedIndex">Refrance to the items selected index</param>
+        /// <param name="showNewDetails">Refrance to the show new details toggle</param>
         private void ListView<T>(ScriptableObjectDatabase<T> database, ref T dbo, ref Vector2 listScrollPosition, ref int selectedIndex, ref bool showNewDetails) where T : DatabaseObject
         {
             // Only display the list if the display state == DisplayState.NONE
@@ -27,8 +33,9 @@ namespace BurgZergArcade.ItemSystem.Editor
             {
                 if (GUILayout.Button(database.Get(cnt).name, "Box", GUILayout.Width(_listViewButtonWidth), GUILayout.Height(_listViewButtonHeight)))
                 {
-                    //Debug.Log(database.Get(cnt).name + " : " + cnt);
                     selectedIndex = cnt;
+
+                    //Debug.Log(database.GetType().ToString());
 
                     //Clone the Item so we are not working with the copy in the database, in order to save these values have to click save
                     if (database is ISWeaponDatabase)
@@ -44,15 +51,13 @@ namespace BurgZergArcade.ItemSystem.Editor
                         dbo = new ISConsumable(DatabaseManager.consumableDatabase.Get(cnt)) as T;
                     }
 
-                    //Debug.Log(database.GetType().ToString());
-
                     showNewDetails = true;
                     displayState = DisplayState.DETAILS;
-                }
-            }
+                }//Item Button
+            }//database loop
 
             // end the Scroll view
             EditorGUILayout.EndScrollView();
-        }
+        }//listview
     }//class
 }//namespace
